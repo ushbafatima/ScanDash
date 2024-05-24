eb2454b05867dafd61859a2d611eb615924b77fa
ushbafatima <155233348+ushbafatima@users.noreply.github.com>
2024-05-24 22:22:08 +0500

    Delete src directory

--- src/AddDescriptionWindow.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JButton;
-
-public interface AddDescriptionWindow {
-	JButton addDescriptionbtn = new JButton();
-	public Boolean isEmptyFields();
-	public Boolean validateDescriptions();
-}
